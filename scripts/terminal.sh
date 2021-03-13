88ef5ff5141b2a407fc388977a6e59a92f97c63f
mtorials <git@mtorials.de>
2021-03-13 14:34:31 +0100

    better script

--- scripts/terminal.sh
+++ scripts/terminal.sh
@@ -3,7 +3,13 @@ echo "Installing Terminal Configs"
 echo "You can find your old configs at ~/backup-configs"
 mkdir ~/config-backups
 
-sudo pacman -S ttf-anonymous-pro
+echo "Installing dependencies"
+sudo pacman -S ttf-anonymous-pro python-pip fish
+
+echo "Changing your shell to fish"
+chsh -s /usr/bin/fish
+
+echo "Backing up"
 cp -r ~/.config/xfce4/terminal ~/config-backups
 rm -r ~/.config/xfce4/terminal
 ln -rs xfce4/terminal ~/.config/xfce4/terminal
