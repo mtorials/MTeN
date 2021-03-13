1f9dd35b03a73eaabf929076ecdf77dc7b82f50b
mtorials <git@mtorials.de>
2021-03-13 15:10:36 +0100

    fix

--- scripts/install-env.sh
+++ scripts/install-env.sh
@@ -1,5 +1,5 @@
 sudo pacman -S xfce4 matcha-gtk-theme papirus-icon-theme
-sudo yay -S picom picom-tryone-git
+sudo yay -S picom-tryone-git
 xfconf-query -c xsettings -p /Net/ThemeName -s "Matcha-sea"
 xfconf-query -c xsettings -p /Net/IconThemeName -s "Papirus"
 mkdir ~/.config/picom
