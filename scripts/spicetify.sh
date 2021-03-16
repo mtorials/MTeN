07ac388a90dc5c72d252cb6d077de53ba69455c6
mtorials <git@mtorials.de>
2021-03-16 16:20:09 +0100

    spiceify scripts

--- scripts/spicetify.sh
+++ scripts/spicetify.sh
@@ -1,4 +1,7 @@
-echo "The following script may not work!!!!!"
+echo "Does not work with Spotify installed as Snap/etc"
 yay -S spicetify-cli
+sudo chmod a+wr /opt/spotify
+sudo chmod a+wr /opt/spotify/Apps -R
+rm -r ~/.config/spicetify/Themes
 ln -rs spicetify-themes/ ~/.config/spicetify/Themes
 spicetify apply
