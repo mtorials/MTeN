c06591e356a2caf2d107eed01a4caf4a7cd4524d
mtorials <git@mtorials.de>
2021-03-18 19:32:13 +0100

    change script

--- scripts/install-env.sh
+++ scripts/install-env.sh
@@ -1,6 +1,6 @@
 sudo pacman -S arc-gtk-theme
 sudo pacman -S papirus-icon-theme
-yay -S picom-tryone-git
+yay -S picom-ibhagwan-git
 xfconf-query -c xsettings -p /Net/ThemeName -s "Arc-Darker"
 xfconf-query -c xsettings -p /Net/IconThemeName -s "Papirus"
 mkdir ~/.config/picom
